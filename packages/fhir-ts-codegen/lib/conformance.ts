cc39576ea8804a8a2db0ad9d3ea96c3df7fe81d5
Andrew Tang <andrew.yishiou.tang@gmail.com>
2019-02-17 13:00:45 -0600

    chore(fhir-ts-codegen): update b64Binary spelling
    Update to use the same spelling as primitives base64Binary type.

--- packages/fhir-ts-codegen/lib/conformance.ts
+++ packages/fhir-ts-codegen/lib/conformance.ts
@@ -3,7 +3,7 @@
  */
 
 export enum FHIRPrimitives {
-  base64binary = "base64binary",
+  base64Binary = "base64Binary",
   boolean = "boolean",
   canonical = "canonical",
   code = "code",
@@ -23,6 +23,32 @@ export enum FHIRPrimitives {
   url = "url",
   uuid = "uuid"
 }
+
+/**
+ * Map from FHIR Primitive type name to FHIR io-ts primitive type
+ * TODO: Move this to primitives package
+ */
+export const FHIRPrimitivesTypes: { [index: string]: string } = {
+  base64Binary: "Base64BinaryType",
+  boolean: "BooleanType",
+  canonical: "CanonicalType",
+  code: "CodeType",
+  date: "DateType",
+  dateTime: "DateTimeType",
+  decimal: "DecimalType",
+  id: "IDType",
+  instant: "InstantType",
+  integer: "IntegerType",
+  markdown: "MarkdownType",
+  oid: "OIDType",
+  positiveInt: "PositiveIntegerType",
+  string: "StringType",
+  time: "TimeType",
+  unsignedInt: "UnsignedIntegerType",
+  uri: "URIType",
+  url: "URLType",
+  uuid: "UUIDType"
+};
 
 export interface StructureDefinition {
   resourceType: string;
@@ -57,4 +83,13 @@ export interface ElementDefinition {
   }>;
   short?: string;
   contentReference?: string;
+}
+
+/**
+ * Logical grouping of element definitions, e.g. a BackboneElement and it's elements
+ */
+export interface ElementGroup {
+  name: string;
+  comment: string;
+  definitions: ElementDefinition[];
 }
